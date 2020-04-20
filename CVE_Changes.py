14f053946d155d336dff2dd84e6f03b409bfa5b6
LukeMcSherry <lmcsherry@flexera.com>
2020-04-20 15:50:54 +0100

    Merge 'cve_format.py' Functionality & Tidy Output

--- CVE_Changes.py
+++ CVE_Changes.py
@@ -1,13 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = ""
-request = requests.get(URL)
-web_page = request.content
+cve_list = []
+cve_format_list = []
+print("Enter CVEs (Separated by New Line, ^D to End:")
+while True:
+    try:
+        cve_input = input()
+    except EOFError:
+        break
+    cve_list.append(cve_input)
 
-soup = BeautifulSoup(web_page, 'lxml')
-text = soup.find_all(text=True)
-cve = soup.select_one("span[data-testid=page-header-vuln-id]").text
-cve_description = soup.select_one("p[data-testid='vuln-description']").text
+print("\n\n---CVE List---\n")
+for cve in cve_list:
+    cve = "CVE-{}".format(cve)
+    cve_format_list.append(cve)
 
-print(cve, ":", cve_description)
+for cve in cve_format_list:
+    print(cve)
+
+print("\n\n---CVE Description---")
+for cve in cve_format_list:
+    URL = "https://nvd.nist.gov/vuln/detail/{}".format(cve)
+    request = requests.get(URL)
+    web_page = request.content
+
+    soup = BeautifulSoup(web_page, 'lxml')
+    text = soup.find_all(text=True)
+    cve_identifier = soup.select_one("span[data-testid=page-header-vuln-id]").text
+    cve_description = soup.select_one("p[data-testid='vuln-description']").text
+    print("\n"+cve_identifier, ":", cve_description)
