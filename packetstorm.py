063e41d7cf4b2516e6925aa2d8dba083ab6f90ac
LukeMcSherry <lmcsherry@flexera.com>
2020-05-20 15:12:08 +0100

    Added Argument Parsing & Additional Handling

--- packetstorm.py
+++ packetstorm.py
@@ -2,18 +2,20 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import argparse
 
 url_list = []
-print("Enter Feed (^D to End):")
-while True:
-    try:
-        packetstorm_input = input()
-    except EOFError:
-        break
-    url_list.append(packetstorm_input)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+args = parser.parse_args()
+
+with open(args.filename) as file:
+    for line in file:
+        url_list.append(line)
 
 url_list = ','.join(map(str, url_list))
-strip_text = ['TITLE : Comments (0),', 'TITLE : Comments (0)', 'TITLE : View', 'TITLE : View,', ',']
+strip_text = ['TITLE : Comments (0),', 'TITLE : Comments (0)', 'TITLE : View', 'TITLE : View,', ',', '\\', '}']
 
 for i in strip_text:
     for item in url_list:
@@ -22,7 +24,7 @@ for i in strip_text:
 url_list = re.split(r'URL\s*:', url_list)
 del url_list[0]
 
-url_list = [x.strip() for x in url_list]
+url_list = [item.strip() for item in url_list]
 
 print("\n---Packetstorm Descriptions---\n")
 for url in url_list:
